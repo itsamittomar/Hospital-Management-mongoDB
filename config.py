afa431f54748a749fa356560e62c0808b8cc83b0
Amit Singh Tomar <49031408+itsamittomar@users.noreply.github.com>
2020-07-03 06:12:07 +0530

    Update config.py
    change in config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 #py -c "import os; print(os.urandom(16))"
 class email(object):
 		
-	EMAIL="abchospitalpvt@gmail.com"
-	EMAIL_PASSWORD="Abchospital@#123"
+	EMAIL="your email id here"
+	EMAIL_PASSWORD="email_id password"
 
 class Config(object):
     SECRET_KEY=os.environ.get("SECRET_KEY") or b'\x1d\xe6\xf23=\xebr\x9a\xf17\xb8<4\x08\x9cf'
